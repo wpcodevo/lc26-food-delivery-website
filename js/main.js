86062ef8dfc173ff1239438cfc05fce85781b595
Edem <ziddah.edem@gmail.com>
2021-11-13 18:58:38 +0000

    updated

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ hamburger.addEventListener("click", () => {
 close.addEventListener("click", () => {
   menu.classList.remove("show");
 });
+
+/* ========== SignIn Form =========== */
+const signInForm = document.querySelector("header .wrapper");
+
+document.querySelector(".signin").onclick = () => {
+  signInForm.classList.add("active");
+};
+
+document.querySelector(".close-form").onclick = () => {
+  signInForm.classList.remove("active");
+};
