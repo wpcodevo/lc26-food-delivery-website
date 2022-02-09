703dde2c60d736154f7f4fb1c76ad2bef65b6cfb
Edem <ziddah.edem@gmail.com>
2022-02-09 17:57:44 +0000

    updated

--- js/testimonial.js
+++ js/testimonial.js
@@ -1,7 +1,7 @@
 /* ========== Get Testimonials =========== */
 const getTestimonials = async () => {
   try {
-    const results = await fetch("./data/testimonials.json");
+    const results = await fetch('./data/testimonials.json');
     const data = await results.json();
     const testimonials = data.testimonials;
     return testimonials;
@@ -10,10 +10,10 @@ const getTestimonials = async () => {
   }
 };
 
-const testimonialsWrapper = document.querySelector(".test-wrapper");
-const cards = [...document.querySelectorAll(".testimonials .card")];
+const testimonialsWrapper = document.querySelector('.test-wrapper');
+const cards = [...document.querySelectorAll('.testimonials .card')];
 
-window.addEventListener("DOMContentLoaded", async function () {
+window.addEventListener('DOMContentLoaded', async function () {
   const testimonials = await getTestimonials();
   displayTestimonials(testimonials);
   filter();
@@ -46,28 +46,28 @@ const displayTestimonials = (items) => {
                   `
   );
 
-  testimonials = testimonials.join("");
+  testimonials = testimonials.join('');
   testimonialsWrapper.innerHTML = testimonials;
 };
 
 /* ========== Filter Testimonials =========== */
 function filter() {
-  const testimonial = [...document.querySelectorAll(".testimonial")];
+  const testimonial = [...document.querySelectorAll('.testimonial')];
   for (let i = 0; i < cards.length; i++) {
-    cards[0].classList.add("active");
-    testimonial[0].classList.add("active");
+    cards[0].classList.add('active');
+    testimonial[0].classList.add('active');
     cards[i].onclick = function () {
       for (let x = 0; x < cards.length; x++) {
-        cards[x].classList.remove("active");
+        cards[x].classList.remove('active');
       }
-      this.classList.add("active");
-      const filter = this.getAttribute("data-filter");
+      this.classList.add('active');
+      const filter = this.getAttribute('data-filter');
       for (let z = 0; z < testimonial.length; z++) {
-        testimonial[z].style.bottom = "-80%";
+        testimonial[z].style.bottom = '-80%';
         testimonial[z].style.opacity = 0;
 
-        if (testimonial[z].getAttribute("data-id") === filter) {
-          testimonial[z].style.bottom = "0%";
+        if (testimonial[z].getAttribute('data-id') === filter) {
+          testimonial[z].style.bottom = '0%';
           testimonial[z].style.opacity = 1;
         }
       }
